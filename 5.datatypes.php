ea3b81622cc7f7a3c9cf33f7a3891141261528a5
unknown <syedrakesh.me@gmail.com>
2021-06-13 23:13:32 +0600

    Datatypes - Exclude Object & Reference Type Datatypes

---
+++ 5.datatypes.php
@@ -0,0 +1,35 @@
+<?php
+    $variable1 = 1;
+    $variable2 = 1.1;
+    $variable3 = true;
+    $variable4 = 'Syed Rakesh';
+    $variable5 = "Syed Rakesh";
+    $variable6 = array('Syed','Rakesh');
+    $variable7 = array("Syed","Rakesh");
+    $variable8 = null;
+    var_dump($variable1);
+    echo '<br>';
+
+    var_dump($variable2);
+    echo '<br>';
+
+    var_dump($variable3);
+    echo '<br>';
+
+    var_dump($variable4);
+    echo '<br>';
+
+    var_dump($variable5);
+    echo '<br>';
+
+    var_dump($variable6);
+    echo '<br>';
+
+    var_dump($variable7);
+    echo '<br>';
+
+    var_dump($variable8);
+    echo '<br>';
+
+    // Object & Reference type datatypes not shown here.
+?>
