95bb3d8183d92d2bdf12f310c26b9f05df0f1317
unknown <syedrakesh.me@gmail.com>
2021-06-13 23:10:14 +0600

    Vairous Ways of print Function

---
+++ 4.2.print_function.php
@@ -0,0 +1,24 @@
+<?php
+    //  print is slower than echo funtion
+    //  print has return
+    //  print function doesn't support comma
+
+    print 'Hello World! <br>';
+    print "Hello World! <br>";
+    
+    //print 'Hello',' World!','<br>'; // Comma not allowed here
+    //print 'Hello' , ' World!' , '<br>'; // Comma not allowed here
+
+    print 'Hello'.' World!'.'<br>';
+    print 'Hello' . ' World!' . '<br>';
+
+    print "Hello"." World!"."<br>";
+    print "Hello" . " World!" . "<br>";
+
+    print ('Hello World! <br>');
+    print ("Hello World! <br>");
+
+    print ('Hello'.' World!'.'<br>'); // Comma not allowed here
+
+    print "I'm Syed Rakesh Uddin <br>";
+?>
