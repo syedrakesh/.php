fd3cac017506f3033ca4bba2c9eb1120b176f4ec
unknown <syedrakesh.me@gmail.com>
2021-06-13 20:03:44 +0600

    Echo & Comment

---
+++ 1.syntax.php
@@ -0,0 +1,13 @@
+<?php
+    echo "Hello World! with echo <br>";
+    Echo "Hello World! with Echo <br>";
+    ECho "Hello World! with ECho <br>";
+    ECHo "Hello World! with ECHo <br>";
+    ECHO "Hello World! with ECHO <br>";
+
+    $color = "Black";
+    $COLOR = "White";
+
+    echo "I Love $color"."<br>";
+    echo "I Love $COLOR"."<br>";
+?>
---
+++ 2.comments.php
@@ -0,0 +1,10 @@
+<?php
+//  Single-Line Comment
+
+/*
+    Multiple-Line Comments
+*/
+
+    $number1 = 5+/*+200*/5; //  also use comments to leave out parts of a code line
+    echo "$number1";
+?>
