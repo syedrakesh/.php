47415647cddc6c41989234cea3acc24ac2c243f1
unknown <syedrakesh.me@gmail.com>
2021-06-13 23:07:50 +0600

    Vairous Ways of echo Function

---
+++ 4.1.echo_function.php
@@ -0,0 +1,24 @@
+<?php
+    //  echo is faster than print funtion
+    //  echo has no return
+    //  echo function support comma but not all the time
+
+    echo 'Hello World! <br>';
+    echo "Hello World! <br>";
+    
+    echo 'Hello',' World!','<br>';
+    echo 'Hello' , ' World!' , '<br>';
+
+    echo 'Hello'.' World!'.'<br>';
+    echo 'Hello' . ' World!' . '<br>';
+
+    echo "Hello"." World!"."<br>";
+    echo "Hello" . " World!" . "<br>";
+
+    echo ('Hello World! <br>');
+    echo ("Hello World! <br>");
+
+    echo ('Hello'.' World!'.'<br>'); // Comma not allowed here
+
+    echo "I'm Syed Rakesh Uddin <br>";
+?>
